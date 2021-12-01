3678e608c0c6e935d46b665d50987783a66f1ee4
shootinstar000 <93067533+shootinstar000@users.noreply.github.com>
2021-12-01 17:39:43 +0400

    update configs.py

--- configs.py
+++ configs.py
@@ -12,7 +12,7 @@ class Config(object):
 	BOT_OWNER = int(os.environ.get("BOT_OWNER","879778871"))
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://lilbrat:lilbrat@cluster0.c9kkp.mongodb.net/myFirstDatabase?retryWrites=true")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", -1001312930387)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
